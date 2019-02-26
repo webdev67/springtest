8b961949b3b5781ee6fe7146764ae0ef16d9d7c4
Rami Mikhail <webdev67@gmail.com>
2019-02-25 21:14:39 -0500

    Added frontend code and angular material support.

--- src/spring/spring/HelloController.java
+++ src/spring/spring/HelloController.java
@@ -1,14 +1,25 @@
 package spring;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-
+	private String response;
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map json() {
+    	String[] myarray = new String[] {"a","b","c","d"};
+        return Collections.singletonMap("response", myarray);
     }
 
 }
